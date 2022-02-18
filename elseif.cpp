0d579860797c737391de02bfb2f8b4fe83ba4f74
Ashish <ashishsam71@gmail.com>
2022-02-18 22:07:48 +0530

    add code

---
+++ elseif.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+    if (x==y)
+    {
+      cout<<"x is equal to y";
+    }
+    else if (x>y)
+    {
+        cout<<"x is greater than y";
+    }
+    else{
+        cout<<"y is greater than x";
+    }
+    return 0;
+    
+    
+}
---
+++ if.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cin>>age;
+    if (age>=18)
+    {
+        cout<<"you can vote";
+    }
+    else{
+        cout<<"you cannot elegible";
+    }
+    return 0;
+}
